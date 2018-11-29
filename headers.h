fa8f8aaa73882bfc13d55f8f2a80029136b67a43
wafistos <mameri.wafi@gmail.com>
2018-11-29 09:19:28 +0100

    Ajout des fichier main et headers

---
+++ headers.h
@@ -0,0 +1,11 @@
+#ifndef __HEADERS__
+#define __HEADERS__
+#include <iostream>
+
+void Coucou(){
+
+    std::cout << "Je dis coucou";
+}
+
+
+#endif // __HEADERS__
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "headers.h"
+using namespace std;
+
+int main(){
+
+    cout << "Bonjour!! je vous Salut depuis mon PC" << endl;
+    Coucou();
+
+    return 0;
+}
